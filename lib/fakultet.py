e69d7e242b0df9dd4ce8ff2f6c417c811d249099
itgsod-Michael-Mathsson <michael.mathsson@elev.it-gymnasiet.se>
2014-10-02 12:52:17 +0200

    Det här gick ju bra

---
+++ lib/fakultet.py
@@ -0,0 +1,12 @@
+def fakultet(nmr):
+    fakultet = nmr
+    if nmr == 0:
+        return 1
+    else:
+        while nmr > 1:
+            nmr = nmr - 1
+            fakultet = fakultet * nmr
+        return fakultet
+
+
+
